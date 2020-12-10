806aeacc583f282ba4fb4c7fb6bd880918adda29
aya_gomaa <aya.gomaa@gmail.com>
2020-12-10 13:08:28 +0200

    placeholder model field

--- ayasite/polls_cms_integration/admin.py
+++ ayasite/polls_cms_integration/admin.py
@@ -1,3 +1,11 @@
+from .models import MyModel
 from django.contrib import admin
+from cms.admin.placeholderadmin import PlaceholderAdminMixin
+
+
+class MyModelAdmin(PlaceholderAdminMixin, admin.ModelAdmin):
+    pass
+
 
 # Register your models here.
+admin.site.register(MyModel, MyModelAdmin)
--- ayasite/polls_cms_integration/models.py
+++ ayasite/polls_cms_integration/models.py
@@ -1,5 +1,7 @@
 from cms.models import CMSPlugin
+from cms.models.fields import PlaceholderField
 from django.db import models
+from parler.models import TranslatableModel, TranslatedFields
 from polls.models import Poll
 
 
@@ -9,3 +11,9 @@ class PollPluginModel(CMSPlugin):
 
     def __str__(self):
         return self.poll.question
+
+
+class MyModel(models.Model):
+    title = models.CharField(null=True, max_length=200)
+    description = models.TextField(null=True)
+    my_placeholder = PlaceholderField('my_placeholder')
